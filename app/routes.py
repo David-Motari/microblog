8f8f1ca2c6adcca4a5eb879d7eeed79df0f30fdd
David Motari <davemotari22@gmail.com>
2024-03-05 14:15:38 +0300

    Added templates for base HTML file and home

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,18 @@
+from flask import render_template
 from app import app
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return "Hello, World!"
+    user = {'username': 'David'}
+    posts = [
+        {
+           'author': { 'username': 'David'},
+           'body': 'A beautiful day in Sobea'
+        },
+        {
+           'author': { 'username': 'Davin'},
+           'body': 'A beautiful day in Ngata'
+        }
+    ]
+    return render_template('index.html', title='Home', user=user, posts=posts)
